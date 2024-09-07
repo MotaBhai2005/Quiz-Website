56f8f0adc6fb40ffce95b1e7d1da87a9bda62f3f
MotaBhai2005 <rudrababulu2005@gmail.com>
2024-09-07 22:25:38 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Who invented C programming language?",
+        answer: "Dennis M. Ritchie",
+        options: [
+            "Brian Wilson Kernighan",
+            "Kenneth Lane Thompson",
+            "Dennis M. Ritchie",
+            "Alan Mathison Turing"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does HTML stands for?",
+        answer: "HyperText Markup Language",
+        options: [
+            "HyperText Markdown Language",
+            "HyperText Multi Language",
+            "HomeText Markplace Language",
+            "HyperText Markup Language"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the binary code for number 15?",
+        answer: "1111",
+        options: [
+            "1000",
+            "0101",
+            "0011",
+            "1111"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What CSS stands for?",
+        answer: "Cascading Style Sheets",
+        options: [
+            "Code Style Sheets",
+            "Computer Style Sheet",
+            "Cascading Style Sheets",
+            "Cute Style Sheets"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Who developed JavaScript?",
+        answer: "Brendan Eich",
+        options: [
+            "Håkon Wium Lie",
+            "Brendan Eich",
+            "Alan Mathison Turing",
+            "Tim Berners-Lee"
+        ]
+    },
+];
